e88c857a203a6ce3cadbb2a31d5976f7afcbe449
Mani Aggarwal <aggarwalmani048@gmail.com>
2022-08-28 13:34:22 +0530

    Initial Commit

---
+++ CardTussleGame/CardTussleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardTussleGameApp.swift
+//  CardTussleGame
+//
+//  Created by Mani Aggarwal on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CardTussleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
